1f282d31fdf767d147470bf4e43697a62fac3d83
kimgiwoong127 <kimgiwoong127@gmail.com>
2026-05-03 07:47:54 +0900

    [QUIZ][IS003] 피드백 수정

--- quiz/quiz/src/main/java/kaguya/domain/question/model/dto/response/QuestionResponse.java
+++ quiz/quiz/src/main/java/kaguya/domain/question/model/dto/response/QuestionResponse.java
@@ -2,32 +2,14 @@ package kaguya.domain.question.model.dto.response;
 
 import java.time.OffsetDateTime;
 
-import kaguya.domain.question.model.entity.QuestionEntity;
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-public class QuestionResponse {
-    private Long idx;
-    private Long quizIdx;
-    private String problemText;
-    private String problemUrl;
-    private String type;
-    private Integer sortOrder;
-    private OffsetDateTime createdDate;
-    private OffsetDateTime updatedDate;
-
-    public static QuestionResponse fromEntity(QuestionEntity question) {
-        return QuestionResponse.builder()
-                .idx(question.getIdx())
-                .quizIdx(question.getQuizIdx())
-                .problemText(question.getProblemText())
-                .problemUrl(question.getProblemUrl())
-                .type(question.getType())
-                .sortOrder(question.getSortOrder())
-                .createdDate(question.getCreatedDate())
-                .updatedDate(question.getUpdatedDate())
-                .build();
-    }
+public record QuestionResponse(
+        Long idx,
+        Long quizIdx,
+        String problemText,
+        String problemUrl,
+        String type,
+        Integer sortOrder,
+        OffsetDateTime createdDate,
+        OffsetDateTime updatedDate
+) {
 }
