34fa44bdc37154cf83cb1a25e356cd8cc44d60d4
JammySeok <jjh326326@naver.com>
2026-07-08 13:34:04 +0900

    [Chat][IS012][SUB002] 저번에 했던 테스트 코드 옮기기

---
+++ chat_spring/src/main/java/kaguya/chat_spring/WebSoket/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package kaguya.chat_spring.WebSoket.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatHandler chatHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // 클라이언트가 'ws://서버주소/ws/raw'로 접속하면 핸들러가 작동하도록 매핑
+        registry.addHandler(chatHandler, "/ws/raw")
+                .setAllowedOrigins("*");  // 테스트용
+    }
+}
